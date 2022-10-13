2fbc66db1dfeb87efe7b8c04cc36f8f5ff2c748f
malikamar104700 <malikamar104800@gmail.com>
2022-10-13 09:07:27 +0500

    Array practice

--- app.js
+++ app.js
@@ -53,13 +53,6 @@ function secondLargestElem(a) {
 }
 secondLargestElem([3, 5, 7, 9, 13]);
 // Program # 6. Write a ts program to count total number of even and odd elements in an array.
-// var array4:number[]=[2,5,23,45,24]
-//     for(var i=0;i<array4.length;i++){
-//         let a=array4[i]
-//         if(a%2==0){
-//             console.log("this is even array",a)
-//         }
-//     }
 function CountingEvenOdd(array, arraysize) {
     var evenCount = 0;
     var oddCount = 0;
@@ -74,3 +67,87 @@ function CountingEvenOdd(array, arraysize) {
 var arr = [2, 3, 4, 5, 6];
 var n = arr.length;
 CountingEvenOdd(arr, n);
+// Program # 7. Write a ts program to count total number of negative elements in an array.
+var myArray = [4, -5, 10, 2, -60, -8, 10, -34];
+function getNegativeNumbers(array) {
+    var negatives = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            negatives.push(array[i]);
+        }
+    }
+    return negatives.length;
+}
+console.log(getNegativeNumbers(myArray), "Negative Elements in array");
+// Program # 8. Write a ts program to copy all elements from an array to another array.
+var a = ["birds", "Parrot"];
+var b = a.slice();
+console.log(b);
+// Program # 9. Write a ts program to insert an element in an array.
+function insertElement() {
+    var array = [61, 29, 12, 32];
+    array.push(5, 12);
+    console.log(array);
+}
+insertElement();
+// Program # 10. Write a ts program to delete an element from an array at specified position.
+var array3 = ["Amar", "Rehmn", "Ahmad", "Rehan", "Bilal"];
+var array4 = array3.pop();
+console.log("Remove element:", array4);
+console.log("Remaining Elemnts:", array3);
+// Program # 12. Write a ts program to print all unique elements in the array.
+function getUniqueArray(array) {
+    var array5 = array.filter(function (element, index, array) { return array.indexOf(element) === index; });
+    return array5;
+}
+var array6 = ["Hello", "This", "Is", "Language", "This", "Is"];
+console.log("Print all unique elements", getUniqueArray(array6));
+//   Program # 13. Write a ts program to count total number of duplicate elements in an array.
+var counts = [];
+var sampleArray = ['a', 'a', 'b', 'c', "c", "d"];
+sampleArray.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+console.log(counts);
+// Program # 15. Write a ts program to merge two array to third array.
+var array7 = ["Amar"];
+var array8 = ["Rehman"];
+var array9 = array7.concat(array8);
+console.log(array9);
+// Program # 16. Write a ts program to find reverse of an array.
+var array10 = ["Moeen", "Jawad", "Muneeb", "Ali"];
+var array11 = array10.reverse();
+console.log(array11);
+// Program # 17. Write a ts program to put even and odd elements of array in two separate array.
+var array12 = [1, 2, 3, 4, 5, 6, 8, 9];
+function seperateArray(array) {
+    var even = array.filter(function (num) { return num % 2 === 0; });
+    var odd = array.filter(function (num) { return num % 2 != 0; });
+    return { odd: odd, even: even };
+}
+console.log(seperateArray(array12));
+// Program # 18. Write a ts program to search an element in an array.
+var array14 = [1, 32, 45, 13];
+var array15 = 32;
+function findElem(elem) {
+    for (var i = 0; i < array14.length; i++) {
+        if (array14[i] === array15) {
+            return "Search Found";
+        }
+    }
+    return "search not found";
+}
+console.log(findElem(array15));
+// program # 19. Write a ts program to sort array elements in ascending or descending order.
+var array16 = [21, 2100, 2, 35000];
+var array17 = [21, 2100, 2, 35000];
+var ascN = array16.sort(function (f, s) { return f - s; });
+var dscN = array17.sort(function (f, s) { return s - f; });
+console.log("Ascending:", ascN);
+console.log("Descending:", dscN);
+// Program # 20. Write a ts program to sort even and odd elements of array separately.
+var array18 = [2, 5, 7, 8, 9, 12];
+function seperate(array) {
+    var even = array.filter(function (num) { return num % 2 === 0; });
+    var odd = array.filter(function (num) { return num % 2 != 0; });
+    return { odd: odd, even: even };
+}
+console.log(seperate(array18));
--- app.ts
+++ app.ts
@@ -61,14 +61,6 @@ function secondLargestElem(a:number[]){
 secondLargestElem([3,5,7,9,13]);
 // Program # 6. Write a ts program to count total number of even and odd elements in an array.
 
-// var array4:number[]=[2,5,23,45,24]
-//     for(var i=0;i<array4.length;i++){
-//         let a=array4[i]
-//         if(a%2==0){
-        
-//             console.log("this is even array",a)
-//         }
-//     }
 function CountingEvenOdd(array:any, arraysize:any)
 {
 	var evenCount:any = 0;
@@ -84,5 +76,99 @@ function CountingEvenOdd(array:any, arraysize:any)
 	var arr = [ 2, 3, 4, 5, 6 ];
 	var n:any = arr.length;
 	CountingEvenOdd(arr,n );
+    
+    // Program # 7. Write a ts program to count total number of negative elements in an array.
 
+    var myArray:number[] = [4, -5, 10, 2, -60, -8, 10, -34 ];
+function getNegativeNumbers(array:number[]) {
+    var negatives:any = [];
+    for (let i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+            negatives.push(array[i]);
+        }
+    }
+    return negatives.length;
+}
+console.log(getNegativeNumbers(myArray),"Negative Elements in array");
 
+// Program # 8. Write a ts program to copy all elements from an array to another array.
+var a:string[] = ["birds" , "Parrot"]
+var b:string []= a.slice()
+console.log(b);
+
+// Program # 9. Write a ts program to insert an element in an array.
+function insertElement() {
+    let array = [61,29,12,32];
+    array.push( 5,12);
+    console.log(array);
+}
+insertElement()
+
+// Program # 10. Write a ts program to delete an element from an array at specified position.
+var array3:string[] = ["Amar", "Rehmn", "Ahmad", "Rehan", "Bilal"]
+var array4:any = array3.pop()
+console.log("Remove element:", array4);
+console.log("Remaining Elemnts:",array3);
+
+// Program # 12. Write a ts program to print all unique elements in the array.
+ function getUniqueArray(array:any){
+    var array5:any = array.filter((element:any, index:any, array:any) => array.indexOf(element) === index);
+    return array5
+  }
+  var array6:any[] = ["Hello", "This", "Is","Language","This", "Is" ];
+  console.log("Print all unique elements", getUniqueArray(array6));
+
+//   Program # 13. Write a ts program to count total number of duplicate elements in an array.
+var counts = []
+var sampleArray:string[] = ['a', 'a', 'b', 'c',"c","d"];
+sampleArray.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+console.log(counts)
+
+// Program # 15. Write a ts program to merge two array to third array.
+var array7:string[] = ["Amar"]
+var array8:string[] = ["Rehman"]
+var array9:any = array7.concat(array8)
+console.log(array9);
+
+// Program # 16. Write a ts program to find reverse of an array.
+var array10:string[] = ["Moeen", "Jawad", "Muneeb","Ali"]
+var array11:any = array10.reverse()
+console.log(array11);
+
+// Program # 17. Write a ts program to put even and odd elements of array in two separate array.
+var array12:number[] = [1, 2, 3, 4,5, 6, 8,9]
+function seperateArray(array:number[]):any{
+var even:any = array.filter(num => num%2===0)
+var odd:any = array.filter(num => num%2 !=0)
+return {odd:odd, even:even}
+}
+console.log(seperateArray(array12));
+// Program # 18. Write a ts program to search an element in an array.
+
+var array14:number[] = [1, 32, 45, 13]
+var array15:number = 32
+function findElem(elem:any) {
+  for (let i = 0; i < array14.length; i++) {
+    if(array14[i]===array15){
+      return "Search Found"
+    }
+  }
+  return "search not found"
+}
+console.log(findElem(array15))
+// program # 19. Write a ts program to sort array elements in ascending or descending order.
+var array16:number[] = [21, 2100, 2, 35000];
+var array17:number[] = [21, 2100, 2, 35000];
+var ascN:any = array16.sort((f, s) => f - s);
+var dscN:any = array17.sort((f, s) => s - f);
+console.log("Ascending:",ascN);
+console.log("Descending:",dscN);
+
+// Program # 20. Write a ts program to sort even and odd elements of array separately.
+var array18:any[] = [2, 5, 7, 8, 9, 12]
+function seperate(array:any):any{
+ var even:any = array.filter(num => num%2===0)
+ var odd:any = array.filter(num => num%2 !=0)
+ return {odd:odd, even:even}
+}
+console.log(seperate(array18))
